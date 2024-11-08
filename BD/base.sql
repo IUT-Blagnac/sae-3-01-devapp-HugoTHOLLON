77c2537aec2b52a697a3ed5df2060c1e91ee0737
[titouan] <[titouannicolasgauze@gmail.com]>
2024-11-08 12:55:55 +0100

    amélioration code sql

--- BD/base.sql
+++ BD/base.sql
@@ -1,45 +1,63 @@
 CREATE TABLE Client{
     idNumCli NUMBER(5),
-    idPanier NUMBER(6),
+    idCommande NUMBER(6),
     nom VARCHAR(25),
 	prenom VARCHAR(15),
 	adressePostale VARCHAR(50),
 	email VARCHAR(20),
 	telephone CHAR(10),
     CONSTRAINT pk_Client PRIMARY KEY (idNumCli),
-    CONSTRAINT fk_Client_Panier
-		FOREIGN KEY (idPanier) REFERENCES Client(idPanier)
+    CONSTRAINT fk_Client_Commande
+		FOREIGN KEY (idCommande) REFERENCES Client(idCommande)
 }
 
 --INSERT INTO Client VALUES (seq_id_client.NEXTVAL,'Gabin','Jean','3 rue t''as de beaux yeux tu sais, 31700 Blagnac','gabin@free.fr','0512345678','N',1);
 
-CREATE TABLE Panier {
-    idPanier NUMBER(6),
-    idNumCli NUMBER(5),
-    CONSTRAINT pk_Panier PRIMARY KEY (idPanier),
-    CONSTRAINT fk_Panier_Client
-		FOREIGN KEY (idNumCli) REFERENCES Panier(idNumCli)
-}
-
 CREATE TABLE Commande {
-    idPanier NUMBER(6),
-    idComposant NUMBER(6),
-    CONSTRAINT fk_Commande_Panier
-		FOREIGN KEY (idPanier) REFERENCES Panier(idPanier),
-	CONSTRAINT fk_ Commande_Composant
-		FOREIGN KEY (idComposant) REFERENCES Composant(idComposant)
+    idCommande NUMBER(6),
+    idNumCli NUMBER(5),
+    idMethodePayment NUMBER(6),
+    dateCommande DATE,
+    panierActuel BOOLEAN, -- Cette variable ne peut être vrai que pour une seule commande d'un client. Elle représente le panier actuel. 
+                          -- Quand elle est fausse, elle représente des commandes déjà passées ce qui constitue l'historique de commande.
+    CONSTRAINT pk_Commande PRIMARY KEY (idCommande),
+    CONSTRAINT fk_Commande_Client
+		FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
+        CONSTRAINT fk_Commande_MethodePayment
+		FOREIGN KEY (idMethodePayment) REFERENCES MethodePayment(idMethodePayment)
 }
 
-CREATE TABLE Historique {
-    
+CREATE TABLE MethodePayment {
+    idMethodePayment NUMBER(6),
+    methodePayment VARCHAR(15),
+    CONSTRAINT pk_MethodePayment PRIMARY KEY (idMethodePayment)
 }
 
-CREATE TABLE Composant {
-    idComposant NUMBER(6),
-    typeComposant VARCHAR(25),
-    nomComposant VARCHAR(50),
+CREATE TABLE ACommande {
+    idCommande NUMBER(6),
+    idNumProduit NUMBER(6),
+    qte NUMBER(5),
+    CONSTRAINT pk_ACommande PRIMARY KEY (idCommande, idNumProduit)
+    CONSTRAINT fk_Commande_Commande
+		FOREIGN KEY (idCommande) REFERENCES Commande(idCommande),
+	CONSTRAINT fk_ Commande_Produit
+		FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
+}
+
+CREATE TABLE Produit {
+    idNumProduit NUMBER(6),
+    idTypeProduit NUMBER(3),
+    nomProduit VARCHAR(50),
     prix NUMBER(5),
-    CONSTRAINT pk_comosant PRIMARY KEY (idComposant)
+    CONSTRAINT pk_Produit PRIMARY KEY (idNumProduit),
+    CONSTRAINT fk_ Produit_TypeProduit
+		FOREIGN KEY (idTypeProduit) REFERENCES TypeProduit(idTypeProduit)
+}
+
+CREATE TABLE TypeProduit {
+    idTypeProduit NUMBER(3),
+    nomTypeProduit VARCHAR(30),
+    CONSTRAINT pk_TypeProduit PRIMARY KEY (idTypeProduit)
 }
 
 CREATE TABLE Administrateur {
@@ -60,4 +78,25 @@ CREATE TABLE Employer {
 	email VARCHAR(20),
 	telephone CHAR(10),
     CONSTRAINT pk_Client PRIMARY KEY (idNumEmployer)
+}
+
+CREATE TABLE Comporte {
+    idNumProduit1 NUMBER(6),
+    idNumProduit2 NUMBER(6),
+    CONSTRAINT pk_Comporte PRIMARY KEY (idNumProduit1, idNumProduit2)
+    CONSTRAINT fk_Comporte_Produit 
+		FOREIGN KEY (idNumProduit1) REFERENCES Produit(idNumProduit),
+	CONSTRAINT fk_Comporte_Produit 
+		FOREIGN KEY (idNumProduit2) REFERENCES Produit(idNumProduit)
+}
+
+CREATE TABLE Avis {
+    idNumCli NUMBER(6),
+    idNumProduit NUMBER(6),
+    CONSTRAINT pk_Avis PRIMARY KEY (idNumCli, idNumProduit)
+    CONSTRAINT fk_Avis_Client 
+    CONSTRAINT pk_Client PRIMARY KEY (idNumEmployer)
+		FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
+	CONSTRAINT fk_Comporte_Produit 
+		FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
 }
